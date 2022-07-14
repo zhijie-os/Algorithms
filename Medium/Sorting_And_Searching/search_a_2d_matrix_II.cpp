a4237eaff5dc24bc1e897b958bdfeb2e899815d6
Zhijie Xia <zhijiexiacs@gmail.com>
2022-07-14 11:40:25 -0600

    2022-07-14: Search a 2D matrix = O(m+n)

---
+++ Medium/Sorting_And_Searching/search_a_2d_matrix_II.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int m = matrix.size(), n = m ? matrix[0].size() : 0, r = 0, c = n - 1;
+
+        while(r<m&&c>=0){
+            if(matrix[r][c]==target){
+                return true;
+            }
+            else if(matrix[r][c]>target){
+                c--;
+            }
+            else{
+                r++;
+            }
+        }
+
+        return false;
+    }
+};
